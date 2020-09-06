6bf634027e6f4a4d1679e8b981ae2cf398e7aa30
John Kline <john@johnkline.com>
2020-09-06 14:08:13 -0700

    Default mile_per_hour and km_per_hour to no decimal places.

--- install.py
+++ install.py
@@ -144,9 +144,9 @@ class LoopDataInstaller(ExtensionInstaller):
                         },
                         'Units' : {
                             'StringFormats': {
-                                'mile_per_hour': '%.1f',
+                                'mile_per_hour': '%.0f',
                                 'degree_C': '%.1f',
-                                'km_per_hour': '%.1f',
+                                'km_per_hour': '%.0f',
                                 'degree_F': '%.1f',
                             },
                         },
